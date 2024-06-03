9559084d81d7eb5a34192ed9958c0dfdc899989e
kyoungminLee <leegyoungmin994@gmail.com>
2024-06-04 00:12:56 +0900

    나중에 이어서 하기, 파이썬 하고 다시 완강 달리즈아!

--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ const app = express();
 
 console.log("hello");
 
-app.listen(3000);
+app.set("view engine","pug");
+// views의 경로를 입력해준다
+app.set("views",__dirname+"/views");
+
+app.use("/public", express.static(__dirname+"/public"));
+// route 생성, home.pug 를 render
+app.get("/", (req, res) => res.render("home"));
+// express 를 통해 구축된 웹사이트를 app.listen 메소드를 실행할 때 콘솔창에 로컬 주소를 띄우는 함수를 만듦
+const handleListen = () => console.log('Listening on http://localhost:3000')
+app.listen(3000, handleListen);
 
 
